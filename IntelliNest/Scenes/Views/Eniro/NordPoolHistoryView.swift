4a5e363dee53f356820a7ec0d294728d4ebef613
Tobias Laross <tobias.laross@gmail.com>
2023-12-31 08:35:43 +0100

    Improvied electricity price chart

--- IntelliNest/Scenes/Views/Eniro/NordPoolHistoryView.swift
+++ IntelliNest/Scenes/Views/Eniro/NordPoolHistoryView.swift
@@ -31,31 +31,27 @@ struct NordPoolHistoryView: View {
                     .foregroundStyle(.gray)
                     .opacity(0.3)
             }
+            .onTapGesture { location in
+                updateSelectedHour(at: location)
+            }
             .gesture(
                 DragGesture().onChanged { value in
-                    let xPosDragged = value.location.x
-                    if xPosDragged <= 0 {
-                        selectedHour = 0
-                    } else if xPosDragged >= chartWidth {
-                        selectedHour = 23
-                    } else {
-                        selectedHour = Int((23 * xPosDragged / chartWidth).rounded())
-                    }
+                    updateSelectedHour(at: value.location)
                 }
             )
+            .sensoryFeedback(.selection, trigger: selectedHour)
             .chartOverlay { _ in
                 RoundedRectangle(cornerRadius: 12)
                     .overlay {
                         Text(
                             """
-                            \(selectedHour):00
+                            \(selectedHour.description)
                             \(nordPool.price(hour: selectedHour)) öre
                             """
                         )
                         .foregroundStyle(.white)
                         .multilineTextAlignment(.center)
                         .lineLimit(2)
-                        .padding(0)
                     }
                     .foregroundStyle(Color.topGrayColor)
                     .frame(width: 75, height: 60)
@@ -74,15 +70,41 @@ struct NordPoolHistoryView: View {
         .background(Color.topGrayColor)
         .opacity(0.9)
     }
+
+    private func updateSelectedHour(at location: CGPoint) {
+        let xPos = location.x
+        if xPos <= 0 {
+            selectedHour = 0
+        } else if xPos >= chartWidth {
+            selectedHour = 23
+        } else {
+            selectedHour = Int((23 * xPos / chartWidth).rounded())
+        }
+    }
 }
 
 struct NordPoolHistory_Previews: PreviewProvider {
     static var previews: some View {
         var nordPool = NordPoolEntity(entityId: .nordPool)
         nordPool.tomorrowValid = true
-        nordPool.today = [32, 26, 18, 17, 19, 30, 43, 57, 66, 61, 54, 52, 49, 47, 52, 59, 69, 69, 66, 61, 45, 46, 42, 28]
+        nordPool.today = [32, 26, 18, 17, 19, 30, 433, 57, 76, 61, 54, 52, 49, 47, 52, 59, 69, 69, 66, 61, 45, 46, 42, 28]
         nordPool.tomorrow = [25, 17, 12, 13, 19, 22, 30, 39, 42, 69, 69, 75, 77, 79, 83, 86, 91, 103, 103, 96, 84, 70, 60, 61]
 
-        return NordPoolHistoryView(nordPool: nordPool)
+        return ZStack {
+            VStack {
+                Spacer()
+                NordPoolHistoryView(nordPool: nordPool)
+                    .frame(height: 350)
+                    .padding(.bottom, 16)
+                    .padding(.horizontal, 8)
+            }
+            .backgroundModifier()
+        }
+    }
+}
+
+private extension Int {
+    var description: String {
+        self <= 9 ? "0\(self):00" : "\(self):00"
     }
 }
